ace81169a6f59badf6220607c2b7185951f518a9
Demerson Freire <116370448+Demerson-Freire@users.noreply.github.com>
2023-04-29 12:15:48 -0300

    Add files via upload

---
+++ DebitRegister/src/debitregister/ConnectionBD.java
@@ -0,0 +1,19 @@
+package debitregister;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionBD {
+    
+    public static Connection getConnection(){
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://database-1.cc2qow3tse8e.us-east-1.rds.amazonaws.com:5432/postgres","postgres","flamengo971102");
+            
+            return conn;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
